3c92aa2b7b1678d48b2870a5d3c2ae918af6ad76
Viktor Lazarev <taurus101v@gmail.com>
2021-11-28 16:53:46 +0100

    Draw Sun & planets

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-use std::ops::AddAssign;
+use std::ops::{AddAssign, Mul};
 
 use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
 
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
+        .add_plugin(ShapePlugin)
         .insert_resource(ListTimer(Timer::from_seconds(0.24, true)))
         .insert_resource(StateTimer(Timer::from_seconds(0.01, true)))
-        .add_startup_system(add_objects.system())
+        .add_startup_system(setup.system())
         .add_system(calculate_new_state.system())
         .add_system(list_objects.system())
         .run();
@@ -25,7 +27,7 @@ struct Velocity(Vec2);
 
 struct Star;
 
-struct NotStar;
+struct Planet;
 
 #[derive(Clone)]
 struct Mass(f32);
@@ -33,6 +35,7 @@ struct Mass(f32);
 const G: f32 = 6.67e-11;
 const TIME_INTERVAL: f32 = 3600.0;
 const ZERO_ANGLE: Vec2 = Vec2::X;
+const SCALE: f32 = 500.0 / 120e9;
 
 #[derive(Clone)]
 struct Name(String);
@@ -61,7 +64,7 @@ fn list_objects(
 fn calculate_new_state(
     time: Res<Time>,
     mut timer: ResMut<StateTimer>,
-    mut query: Query<(Entity, &Name, &mut Position, &mut Velocity, &Mass), With<Mass>>,
+    mut query: Query<(Entity, &Name, &mut Position, &mut Velocity, &Mass, &mut Transform), With<Mass>>,
 ) {
     if !timer.0.tick(time.delta()).just_finished() {
         return;
@@ -69,11 +72,11 @@ fn calculate_new_state(
 
     let mut prev_state = vec![];
 
-    for (entity, name, position, _, mass) in query.iter_mut() {
+    for (entity, name, position, _, mass, _) in query.iter_mut() {
         prev_state.push((entity.id(), name.clone(), position.clone(), mass.clone()));
     }
 
-    for (entity, name, mut position, mut velocity, _) in query.iter_mut() {
+    for (entity, name, mut position, mut velocity, _, mut transform) in query.iter_mut() {
         for (other_entity_id, other_name, other_position, other_mass) in &prev_state {
             if &entity.id() == other_entity_id {
                 continue;
@@ -95,31 +98,72 @@ fn calculate_new_state(
             // );
         }
         position.0.add_assign(velocity.0 * TIME_INTERVAL);
+        transform.translation = (position.0.mul(SCALE), 0.0).into();
         // println!("{} ({:?}) => [{:?}]", name.0, position.0, velocity.0);
     }
     // println!("------");
 }
 
-fn add_objects(mut commands: Commands) {
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(UiCameraBundle::default());
+
+    let sun_circle = shapes::Circle {
+        radius: 4.0,
+        center: Vec2::new(0.0, 0.0),
+    };
+
     commands
-        .spawn()
+        .spawn_bundle(GeometryBuilder::build_as(
+            &sun_circle,
+            ShapeColors::new(Color::YELLOW),
+            DrawMode::Fill(FillOptions::default()),
+            Transform::default(),
+        ))
         .insert(Star)
         .insert(Name("Sun".to_string()))
         .insert(Position(Vec2::new(0.0, 0.0)))
         .insert(Velocity(Vec2::new(0.0, 0.0)))
         .insert(Mass(1.989e30));
+
+    let init_mercury_pos = Vec2::new(69.817445e9, 0.0);
+
+    let mercury_circle = shapes::Circle {
+        radius: 2.0,
+        center: Vec2::new(0.0, 0.0),
+    };
+    let scaled_mercury_pos = init_mercury_pos.mul(SCALE);
+
     commands
-        .spawn()
-        .insert(NotStar)
+        .spawn_bundle(GeometryBuilder::build_as(
+            &mercury_circle,
+            ShapeColors::new(Color::BLACK),
+            DrawMode::Fill(FillOptions::default()),
+            Transform::from_xyz(scaled_mercury_pos.x, scaled_mercury_pos.y, 0.0),
+        ))
+        .insert(Planet)
         .insert(Name("Mercury".to_string()))
-        .insert(Position(Vec2::new(69.817445e9, 0.0)))
+        .insert(Position(init_mercury_pos))
         .insert(Velocity(Vec2::new(0.0, 38.7e3)))
         .insert(Mass(3.285e23));
+
+    let init_venus_pos = Vec2::new(-108e9, 0.0);
+
+    let venus_circle = shapes::Circle {
+        radius: 2.0,
+        center: Vec2::new(0.0, 0.0),
+    };
+    let scaled_venus_pos = init_venus_pos.mul(SCALE);
     commands
-        .spawn()
-        .insert(NotStar)
+        .spawn_bundle(GeometryBuilder::build_as(
+            &venus_circle,
+            ShapeColors::new(Color::BLACK),
+            DrawMode::Fill(FillOptions::default()),
+            Transform::from_xyz(scaled_venus_pos.x, scaled_venus_pos.y, 0.0),
+        ))
+        .insert(Planet)
         .insert(Name("Venus".to_string()))
-        .insert(Position(Vec2::new(-108e9, 0.0)))
+        .insert(Position(init_venus_pos))
         .insert(Velocity(Vec2::new(0.0, -35.0e3)))
         .insert(Mass(4.867e24));
 }
