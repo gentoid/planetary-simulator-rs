0102bbfd138f9cb8a6fa9faa5968091ed47c2a5c
Viktor Lazarev <taurus101v@gmail.com>
2021-12-03 01:43:46 +0100

    Style toggle look

--- src/ui.rs
+++ src/ui.rs
@@ -13,9 +13,10 @@ impl Plugin for TogglePlugin {
 }
 
 struct ToggleMaterials {
-    enabled: Handle<ColorMaterial>,
-    disabled: Handle<ColorMaterial>,
-    border: Handle<ColorMaterial>,
+    slider_enabled: Handle<ColorMaterial>,
+    slider_disabled: Handle<ColorMaterial>,
+    border_disabled: Handle<ColorMaterial>,
+    border_enabled: Handle<ColorMaterial>,
     bg: Handle<ColorMaterial>,
 }
 
@@ -23,9 +24,10 @@ impl FromWorld for ToggleMaterials {
     fn from_world(world: &mut World) -> Self {
         let mut materials = world.get_resource_mut::<Assets<ColorMaterial>>().unwrap();
         ToggleMaterials {
-            enabled: materials.add(Color::rgb(0.15, 0.15, 0.15).into()),
-            disabled: materials.add(Color::rgb(0.25, 0.25, 0.25).into()),
-            border: materials.add(Color::DARK_GREEN.into()),
+            slider_enabled: materials.add(Color::rgb(0.15, 0.15, 0.15).into()),
+            slider_disabled: materials.add(Color::rgb(0.25, 0.25, 0.25).into()),
+            border_disabled: materials.add(Color::rgb(0.2, 0.2, 0.2).into()),
+            border_enabled: materials.add(Color::DARK_GREEN.into()),
             bg: materials.add(Color::BLACK.into()),
             // pressed: materials.add(Color::rgb(0.35, 0.75, 0.35).into()),
         }
@@ -47,14 +49,19 @@ fn draw(mut commands: Commands, materials: Res<ToggleMaterials>) {
     // ui_bundle.transform.translation = Vec3::new(0.0, 0.0, 0.0);
     // commands.spawn_bundle(ui_bundle);
 
+    let root_size = (Val::Px(40.0), Val::Px(20.0));
+    let border_width = Val::Px(1.0);
+    let toggle_padding = Val::Px(3.0);
+    let slider_width = Val::Px(16.0);
     commands
         // root: border
         .spawn_bundle(NodeBundle {
             style: Style {
-                size: Size::new(Val::Px(25.0), Val::Px(20.0)),
+                size: Size::new(root_size.0, root_size.1),
+                padding: Rect::all(border_width),
                 ..Default::default()
             },
-            material: materials.border.clone(),
+            material: materials.border_enabled.clone(),
             ..Default::default()
         })
         .with_children(|parent| {
@@ -62,7 +69,8 @@ fn draw(mut commands: Commands, materials: Res<ToggleMaterials>) {
             parent
                 .spawn_bundle(NodeBundle {
                     style: Style {
-                        size: Size::new(Val::Px(23.0), Val::Px(18.0)),
+                        size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                        padding: Rect::all(toggle_padding),
                         ..Default::default()
                     },
                     material: materials.bg.clone(),
@@ -70,24 +78,27 @@ fn draw(mut commands: Commands, materials: Res<ToggleMaterials>) {
                 })
                 .with_children(|parent| {
                     // toggle slider: border
-                    parent.spawn_bundle(NodeBundle {
-                        style: Style {
-                            size: Size::new(Val::Px(21.0), Val::Px(16.0)),
-                            ..Default::default()
-                        },
-                        material: materials.border.clone(),
-                        ..Default::default()
-                    }).with_children(|parent| {
-                        // toggle: body
-                        parent.spawn_bundle(NodeBundle {
+                    parent
+                        .spawn_bundle(NodeBundle {
                             style: Style {
-                                size: Size::new(Val::Px(19.0), Val::Px(14.0)),
+                                size: Size::new(slider_width, Val::Percent(100.0)),
+                                padding: Rect::all(border_width),
                                 ..Default::default()
                             },
-                            material: materials.disabled.clone(),
+                            material: materials.border_disabled.clone(),
                             ..Default::default()
+                        })
+                        .with_children(|parent| {
+                            // toggle slider: body
+                            parent.spawn_bundle(NodeBundle {
+                                style: Style {
+                                    size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                                    ..Default::default()
+                                },
+                                material: materials.slider_disabled.clone(),
+                                ..Default::default()
+                            });
                         });
-                    });
                 });
         });
 }
