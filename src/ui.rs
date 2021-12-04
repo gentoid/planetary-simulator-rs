8515a81a37a9667de3c6513fb46515b5426696e6
Viktor Lazarev <taurus101v@gmail.com>
2021-12-04 20:18:40 +0100

    Remove commented out code

--- src/ui.rs
+++ src/ui.rs
@@ -1,11 +1,1 @@
 pub mod toggle_switch;
-
-// pub struct TogglePlugin;
-
-// impl Plugin for TogglePlugin {
-//     fn build(&self, app: &mut App) {
-//         app.init_resource::<ToggleMaterials>()
-//             .add_startup_system(initial_draw.system())
-//             .add_system(switch_toggle.system());
-//     }
-// }
